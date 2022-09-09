0ffce3e6103c90d942ec32f276c22cfe6164e52e
Blaquewithaq <blaquewithaq@gmail.com>
2022-09-09 10:11:46 -0500

    added cargo config and emscripten-linker

--- emscripten.sh
+++ emscripten.sh
@@ -6,6 +6,5 @@ git checkout emscripten
 
 cd ..
 
-export EMCC_CFLAGS="-s USE_SDL=2 -s NO_DISABLE_EXCEPTION_CATCHING -s ERROR_ON_UNDEFINED_SYMBOLS=0"
 
 cargo clean && cargo build --target wasm32-unknown-emscripten
