154b38426d6e09f10b1094fce26cd08c7241c799
Blaquewithaq <blaquewithaq@gmail.com>
2022-11-18 11:59:44 -0600

    safe lib

---
+++ src/lib.rs
@@ -0,0 +1,340 @@
+//! ProjectM for Rust
+//!
+//! This library contains bindings to libprojectm. Its purpose 
+//! is to read an audio input and to produce mesmerizing visuals
+//! by detecting tempo, and rendering advanced equations into a 
+//! limitless array of user-contributed visualizations.
+//! 
+//! # Example
+//!
+//! ```
+//! use projectm_rs::*;
+//! 
+//! let settings = Settings {
+//!     mesh_x: 96,
+//!     mesh_y: 54,
+//!     fps: 30,
+//!     texture_size: 512,
+//!     window_width: 1280,
+//!     window_height: 720,
+//!     preset_duration: 15.0,
+//!     soft_cut_duration: 15.0,
+//!     hard_cut_duration: 60.0,
+//!     hard_cut_enabled: false,
+//!     hard_cut_sensitivity: 0.0,
+//!     beat_sensitivity: 0.5,
+//!     aspect_correction: true,
+//!     easter_egg: 0.5,
+//!     shuffle_enabled: true,
+//!     soft_cut_ratings_enabled: true,
+//!     preset_path: String::from("./presets"),
+//!     texture_path: String::from("./textures"),
+//!     data_path: String::from("./"),
+//!     ;
+//!
+//! let projectm_handle = projectm::create(&settings, 0);
+//! ```
+//! 
+
+
+extern crate libc;
+extern crate projectm_sys as ffi;
+
+use std::ffi::CStr;
+use libc::{c_int};
+
+
+pub enum projectm {}
+pub type projectm_handle = *mut ffi::projectm;
+
+#[repr(C)]
+pub struct Settings {
+    pub mesh_x: u32,
+    pub mesh_y: u32,
+    pub fps: u32,
+    pub texture_size: u32,
+    pub window_width: u32,
+    pub window_height: u32,
+    pub preset_path: String,
+    pub texture_path: String,
+    pub data_path: String,
+    pub preset_duration: f64,
+    pub soft_cut_duration: f64,
+    pub hard_cut_duration: f64,
+    pub hard_cut_enabled: bool,
+    pub hard_cut_sensitivity: f32,
+    pub beat_sensitivity: f32,
+    pub aspect_correction: bool,
+    pub easter_egg: f32,
+    pub shuffle_enabled: bool,
+    pub soft_cut_ratings_enabled: bool,
+}
+pub type settings = Settings;
+
+impl projectm {
+    pub fn create(settings: &settings, flags: i32) -> *mut ffi::projectm {
+
+        let projectm_settings = ffi::projectm_settings {
+            mesh_x: settings.mesh_x as c_int,
+            mesh_y: settings.mesh_y as c_int,
+            fps: settings.fps as c_int,
+            texture_size: settings.texture_size as c_int,
+            window_width: settings.window_width as c_int,
+            window_height: settings.window_height as c_int,
+            preset_path: settings.preset_path.as_bytes().as_ptr() as *mut i8,
+            texture_path: settings.texture_path.as_bytes().as_ptr() as *mut i8,
+            data_path: settings.data_path.as_bytes().as_ptr() as *mut i8,
+            preset_duration: settings.preset_duration,
+            soft_cut_duration: settings.soft_cut_duration,
+            hard_cut_duration: settings.hard_cut_duration,
+            hard_cut_enabled: settings.hard_cut_enabled,
+            hard_cut_sensitivity: settings.hard_cut_sensitivity,
+            beat_sensitivity: settings.beat_sensitivity,
+            aspect_correction: settings.aspect_correction,
+            easter_egg: settings.easter_egg,
+            shuffle_enabled: settings.shuffle_enabled,
+            soft_cut_ratings_enabled: settings.soft_cut_ratings_enabled,
+        };
+
+
+        return unsafe { ffi::projectm_create_settings(&projectm_settings, flags) };
+    }
+    
+    pub fn create_from_file(settings_path: &String, flags: i32) -> *mut ffi::projectm {
+        return unsafe { ffi::projectm_create(settings_path.as_bytes().as_ptr() as *mut i8, flags) };
+    }
+
+    pub fn destroy(instance: projectm_handle) {
+        unsafe { ffi::projectm_destroy(instance) };
+    }
+
+    pub fn get_settings(instance: projectm_handle) -> *mut ffi::projectm_settings {
+        return unsafe { ffi::projectm_get_settings(instance) };
+    }
+
+    pub fn write_config(config_file: String, settings: *const ffi::projectm_settings) {
+        unsafe { ffi::projectm_write_config(config_file.as_bytes().as_ptr() as *mut i8, settings) };
+    }
+
+    // -----------------
+
+    pub fn get_texture_size(instance: projectm_handle) -> usize {
+        return unsafe { ffi::projectm_get_texture_size(instance) };
+    }
+
+    pub fn set_texture_size(instance: projectm_handle, size: usize) {
+        unsafe { ffi::projectm_set_texture_size(instance, size) };
+    }
+
+    pub fn get_beat_sensitivity(instance: projectm_handle) -> f32 {
+        return unsafe { ffi::projectm_get_beat_sensitivity(instance) };
+    }
+
+    pub fn set_beat_sensitivity(instance: projectm_handle, sensitivity: f32) {
+        unsafe { ffi::projectm_set_beat_sensitivity(instance, sensitivity) };
+    }
+
+    pub fn get_hard_cut_duration(instance: projectm_handle) -> f64 {
+        return unsafe { ffi::projectm_get_hard_cut_duration(instance) };
+    }
+
+    pub fn set_hard_cut_duration(instance: projectm_handle, seconds: f64) {
+        unsafe { ffi::projectm_set_hard_cut_duration(instance, seconds) };
+    }
+
+    pub fn get_hard_cut_enabled(instance: projectm_handle) -> bool {
+        return unsafe { ffi::projectm_get_hard_cut_enabled(instance) };
+    }
+
+    pub fn set_hard_cut_enabled(instance: projectm_handle, enabled: bool) {
+        unsafe { ffi::projectm_set_hard_cut_enabled(instance, enabled) }
+    }
+
+    pub fn get_hard_cut_sensitivity(instance: projectm_handle) -> f32 {
+        return unsafe { ffi::projectm_get_hard_cut_sensitivity(instance) };
+    }
+
+    pub fn set_hard_cut_sensitivity(instance: projectm_handle, sensitivity: f32) {
+        unsafe { ffi::projectm_set_hard_cut_sensitivity(instance, sensitivity) }
+    }
+
+    pub fn get_soft_cut_duration(instance: projectm_handle) -> f64 {
+        return unsafe { ffi::projectm_get_soft_cut_duration(instance) };
+    }
+
+    pub fn set_soft_cut_duration(instance: projectm_handle, seconds: f64) {
+        unsafe { ffi::projectm_set_soft_cut_duration(instance, seconds) }
+    }
+
+    // !TODO Add this function
+    // pub fn get_preset_duration(instance: projectm_handle, seconds: f64) {
+    //     unsafe { ffi::projectm_get_preset_duration(instance, seconds) }
+    // }
+
+    pub fn set_preset_duration(instance: projectm_handle, seconds: f64) {
+        unsafe { ffi::projectm_set_preset_duration(instance, seconds) }
+    }
+        
+    pub fn get_mesh_size(instance: projectm_handle) -> [usize; 2] {
+        #[derive(Debug, Default, Copy, Clone)]
+        #[repr(C, packed)]
+        struct Mesh {
+            mesh_x: usize,
+            mesh_y: usize,
+        }
+    
+        let mut mesh = Mesh::default();
+    
+        unsafe {
+            ffi::projectm_get_mesh_size(
+                instance,
+                std::ptr::addr_of_mut!(mesh.mesh_x),
+                std::ptr::addr_of_mut!(mesh.mesh_y),
+            );
+        }
+    
+        return [mesh.mesh_x, mesh.mesh_y];
+    }
+
+    pub fn set_mesh_size(instance: projectm_handle, mesh_x: usize, mesh_y: usize) {    
+        unsafe {
+            ffi::projectm_set_mesh_size(
+                instance,
+                mesh_x,
+                mesh_y,
+            );
+        }
+    }
+    
+    pub fn get_fps(instance: projectm_handle) -> usize {
+        return unsafe { ffi::projectm_get_fps(instance) };
+    }
+
+    // !TODO Add this function
+    // pub fn set_fps(instance: projectm_handle) -> usize {
+    //     return unsafe { ffi::projectm_set_fps(instance) };
+    // }
+
+    pub fn get_preset_path(instance: projectm_handle) -> String {
+        let preset_path = unsafe { ffi::projectm_get_preset_path(instance) };
+    
+        let preset_path_str = unsafe { CStr::from_ptr(preset_path) };
+    
+        return preset_path_str.to_str().unwrap().to_string();
+    }
+
+    pub fn get_texture_path(instance: projectm_handle) -> String {
+        let texture_path = unsafe { ffi::projectm_get_texture_path(instance) };
+    
+        let texture_path_str = unsafe { CStr::from_ptr(texture_path) };
+    
+        return texture_path_str.to_str().unwrap().to_string();
+    }
+
+    pub fn get_data_path(instance: projectm_handle) -> String {
+        let data_path = unsafe { ffi::projectm_get_data_path(instance) };
+    
+        let data_path_str = unsafe { CStr::from_ptr(data_path) };
+    
+        return data_path_str.to_str().unwrap().to_string();
+    }
+
+    pub fn get_aspect_correction(instance: projectm_handle) -> bool {
+        return unsafe { ffi::projectm_get_aspect_correction(instance) };
+    }
+
+    pub fn set_aspect_correction(instance: projectm_handle, enabled: bool) {
+        unsafe { ffi::projectm_set_aspect_correction(instance, enabled) };
+    }
+
+    pub fn get_easter_egg(instance: projectm_handle) -> f32 {
+        return unsafe { ffi::projectm_get_easter_egg(instance) };
+    }
+
+    pub fn set_easter_egg(instance: projectm_handle, sensitivity: f32) {
+        unsafe { ffi::projectm_set_easter_egg(instance, sensitivity) };
+    }
+
+    pub fn get_window_size(instance: projectm_handle) -> [usize; 2] {
+        #[derive(Debug, Default, Copy, Clone)]
+        #[repr(C, packed)]
+        struct Mesh {
+            width: usize,
+            height: usize,
+        }
+    
+        let mut window = Mesh::default();
+    
+        unsafe {
+            ffi::projectm_get_window_size(
+                instance,
+                std::ptr::addr_of_mut!(window.width),
+                std::ptr::addr_of_mut!(window.height),
+            );
+        }
+    
+        return [window.width, window.height];
+    }
+
+    pub fn set_window_size(instance: projectm_handle, width: usize, height: usize) {
+        unsafe { ffi::projectm_set_window_size(instance, width, height) };
+    }
+
+    // -----------------
+
+    pub fn reset_textures(instance: projectm_handle) {
+        unsafe { ffi::projectm_reset_textures(instance) };
+    }
+
+    pub fn render_frame(instance: projectm_handle) {
+        unsafe { ffi::projectm_render_frame(instance) };
+    }
+
+    pub fn init_render_to_texture(instance: projectm_handle) -> u32 {
+        return unsafe { ffi::projectm_init_render_to_texture(instance) };
+    }
+    
+    pub fn select_random_preset(instance: projectm_handle, hard_cut: bool) {
+        unsafe { ffi::projectm_select_random_preset(instance, hard_cut) };
+    }
+
+    // -----------------
+
+    pub fn pcm_get_max_samples() -> u32 {
+        return unsafe { ffi::projectm_pcm_get_max_samples() };
+    }
+
+    pub fn pcm_add_float(
+        instance: projectm_handle,
+        samples: *const f32,
+        count: u32,
+        // channels: projectm_channels, Fix channels
+    ) {
+        unsafe { ffi::projectm_pcm_add_float(instance, samples, count, 2) }
+    }
+
+    pub fn pcm_add_int16(
+        instance: projectm_handle,
+        samples: *const i16,
+        count: u32,
+        // channels: projectm_channels, Fix channels
+    ) {
+        unsafe { ffi::projectm_pcm_add_int16(instance, samples, count, 2) }
+    }
+
+    pub fn pcm_add_uint8(
+        instance: projectm_handle,
+        samples: *const u8,
+        count: u32,
+        // channels: projectm_channels, Fix channels
+    ) {
+        unsafe { ffi::projectm_pcm_add_uint8(instance, samples, count, 2) }
+    }
+
+    // -----------------
+
+    pub fn write_debug_image_on_next_frame(instance: projectm_handle) {
+        unsafe { ffi::projectm_write_debug_image_on_next_frame(instance) };
+    }
+}
+
